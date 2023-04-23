45330f5ae98517365a1acecc3bf3f2a2c0145a04
Max Morris <123582087+mmorrislsu@users.noreply.github.com>
2023-04-23 17:00:15 -0500

    Added unique id to jobs list in TruckerTable.sql

--- TruckerTable.sql
+++ TruckerTable.sql
@@ -13,7 +13,10 @@
 
  Date: 14/04/2023 11:58:16
 */
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; --Insert SQl password to get to post information on board
+CREATE DATABASE IF NOT EXISTS TRUCKER;
+USE TRUCKER;
+
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; --Insert SQl password to get to post information on board
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -32,19 +35,20 @@ CREATE TABLE `trucks_delivering`  (
   `DestinationState` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'Destination State',
   `Miles` int NULL DEFAULT NULL COMMENT 'Miles',
   `TruckCost` int NULL DEFAULT NULL COMMENT 'Truck Cost',
-  `Type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'Type'
+  `Type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'Type',
+  `UniqueJobId` INT AUTO_INCREMENT PRIMARY KEY NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of trucks_delivering
 -- ----------------------------
-INSERT INTO `trucks_delivering` VALUES ('johndoe@example.com', '2015-11-16 00:00:00', 8, 'Flatbed', 'CLEVELAND', 'OH', 'MILFORD', 'CT', 517, 5000, 'Broker');
-INSERT INTO `trucks_delivering` VALUES ('janedoe@example.com', '2015-11-16 00:00:00', 8, 'FLAT OR STEP DECK', 'MARIETTA', 'OH', 'CEDAR RAOIDS', 'IA', 689, 11000, 'Broker');
-INSERT INTO `trucks_delivering` VALUES ('jimsmith@example.com', '2015-11-16 00:00:00', 7, 'VAN', 'BUCYRUS', 'OH', 'DANVILLE', 'IL', 309, NULL, 'Truck');
-INSERT INTO `trucks_delivering` VALUES ('johndoe@example.com', '2023-04-19 10:00:00', 8, 'Refrigerated', 'Boston', 'MA', 'Dallas', 'TX', 1900, 9000, 'Full Truckload');
-INSERT INTO `trucks_delivering` VALUES ('janedoe@example.com', '2023-04-20 08:00:00', 7, 'Dry Van', 'Seattle', 'WA', 'Orlando', 'FL', 3000, 12000, 'Full Truckload');
-INSERT INTO `trucks_delivering` VALUES ('jimsmith@example.com', '2023-04-21 12:00:00', 9, 'Flatbed', 'Portland', 'OR', 'New York', 'NY', 4000, 15000, 'Full Truckload');
-INSERT INTO `trucks_delivering` VALUES ('johnsmith@example.com', '2023-04-22 15:00:00', 12, 'Dry Van', 'Chicago', 'IL', 'Los Angeles', 'CA', 2000, 10000, 'Full Truckload');
-INSERT INTO `trucks_delivering` VALUES ('janesmith@example.com', '2023-04-23 09:00:00', 13, 'Flatbed', 'Miami', 'FL', 'Houston', 'TX', 1200, 8000, 'Full Truckload');
-INSERT INTO `trucks_delivering` VALUES ('johntaylor@example.com', '2023-04-24 14:00:00', 8, 'Refrigerated', 'Atlanta', 'GA', 'San Francisco', 'CA', 3000, 12000, 'Full Truckload');
+INSERT INTO `trucks_delivering` VALUES ('johndoe@example.com', '2015-11-16 00:00:00', 8, 'Flatbed', 'CLEVELAND', 'OH', 'MILFORD', 'CT', 517, 5000, 'Broker', NULL);
+INSERT INTO `trucks_delivering` VALUES ('janedoe@example.com', '2015-11-16 00:00:00', 8, 'FLAT OR STEP DECK', 'MARIETTA', 'OH', 'CEDAR RAOIDS', 'IA', 689, 11000, 'Broker', NULL);
+INSERT INTO `trucks_delivering` VALUES ('jimsmith@example.com', '2015-11-16 00:00:00', 7, 'VAN', 'BUCYRUS', 'OH', 'DANVILLE', 'IL', 309, NULL, 'Truck', NULL);
+INSERT INTO `trucks_delivering` VALUES ('johndoe@example.com', '2023-04-19 10:00:00', 8, 'Refrigerated', 'Boston', 'MA', 'Dallas', 'TX', 1900, 9000, 'Full Truckload', NULL);
+INSERT INTO `trucks_delivering` VALUES ('janedoe@example.com', '2023-04-20 08:00:00', 7, 'Dry Van', 'Seattle', 'WA', 'Orlando', 'FL', 3000, 12000, 'Full Truckload', NULL);
+INSERT INTO `trucks_delivering` VALUES ('jimsmith@example.com', '2023-04-21 12:00:00', 9, 'Flatbed', 'Portland', 'OR', 'New York', 'NY', 4000, 15000, 'Full Truckload', NULL);
+INSERT INTO `trucks_delivering` VALUES ('johnsmith@example.com', '2023-04-22 15:00:00', 12, 'Dry Van', 'Chicago', 'IL', 'Los Angeles', 'CA', 2000, 10000, 'Full Truckload', NULL);
+INSERT INTO `trucks_delivering` VALUES ('janesmith@example.com', '2023-04-23 09:00:00', 13, 'Flatbed', 'Miami', 'FL', 'Houston', 'TX', 1200, 8000, 'Full Truckload', NULL);
+INSERT INTO `trucks_delivering` VALUES ('johntaylor@example.com', '2023-04-24 14:00:00', 8, 'Refrigerated', 'Atlanta', 'GA', 'San Francisco', 'CA', 3000, 12000, 'Full Truckload', NULL);
 SET FOREIGN_KEY_CHECKS = 1;

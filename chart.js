ede8cf8975b9fe1f464b9dc8684552d2d5ecedb4
anelly25 <42612522+anelly25@users.noreply.github.com>
2023-05-04 07:18:11 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chart = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {/* Implement your chart here */}
+    </div>
+  );
+};
+
+export default Chart;
